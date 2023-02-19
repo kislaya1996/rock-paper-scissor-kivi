f5e073333a23a7cc259acbe3bb9645a877357756
kislaya1996 <kislayamishra96@gmail.com>
2023-02-19 13:59:34 +0530

    added docstrings

--- rps.py
+++ rps.py
@@ -9,20 +9,37 @@ class RockPaperScissor:
     user_move = None
 
     @staticmethod
-    def is_valid_move(user_move):
+    def is_valid_move(user_move: str) -> bool:
+        '''
+        Checks if a user input lies in the domain of moves within the game
+        '''
         if user_move.lower() in RockPaperScissor.POSSIBLE_MOVES:
             return True
         return False
 
-    def set_user_move(self, user_move):
+    def set_user_move(self, user_move: str) -> None:
+        '''
+        user_move setter in the game object
+        '''
         self.user_move = user_move
         return
 
-    def make_computer_move(self):
+    def make_computer_move(self) -> None:
+        '''
+        computer_move setter in the game object.
+        Random selection made from within the domain of moves in the game
+        '''
         self.computer_move = random.choice(self.POSSIBLE_MOVES)
         return
     
-    def evaluate(self):
+    def evaluate(self) -> str:
+        '''
+        returns str:
+            The message declaring the result of the game
+
+        Evaluates the user move vs the computer move in the game. 
+        Follows the priority order same the game, ie paper beats rock, rock beats scissor, and scissor beats paper
+        '''
         if not (self.computer_move and self.user_move):
             raise Exception("missing-moves")
         if self.computer_move == self.user_move:
